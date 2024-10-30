535a1fa696e0394f9b71badceb3b700db380e790
EduardoBragaT <luixed18@gmail.com>
2024-10-30 13:23:17 -0400

    Passo a Passo

---
+++ src/scripts/main.js
@@ -0,0 +1,8 @@
+/* Passo 1: Criar dois vetores um de emojis, e outro para guarda cartas abertas */
+/* Passo 2: embaralha emojis e guardar em outra variável*/ 
+/* Passo 3: Cria elemento, adicionar uma carta embaralhada, e dar appendChild no jogo */
+    /* Passo 3.1: adiciona a carta embaralhada  */
+    /* Passo 3.2: adiciona evento onclick para abrir se houver menos de 2 selecionados, abrir a carta, e colocar no vetor de abertos  */
+        /* Passo 3.2.1:  função para testar se as cartas possuem o mesmo emoji, se for igual manter viradas, se for diferentes desvirar a carta*/
+        /* Passo 3.2.2: Limpar vetor das abertas */
+/* Passo 4: Cria condição de vitoria */
