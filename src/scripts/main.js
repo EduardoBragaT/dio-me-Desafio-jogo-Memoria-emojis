aed0ddd1ef69db8156c39a8ff78435286c6221c1
EduardoBragaT <luixed18@gmail.com>
2024-10-30 23:47:58 -0400

    Finalizado desafio

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,158 @@
-/* Passo 1: Criar dois vetores um de emojis, e outro para guarda cartas abertas */
-/* Passo 2: embaralha emojis e guardar em outra variável*/ 
-/* Passo 3: Cria elemento, adicionar uma carta embaralhada, e dar appendChild no jogo */
-    /* Passo 3.1: adiciona a carta embaralhada  */
-    /* Passo 3.2: adiciona evento onclick para abrir se houver menos de 2 selecionados, abrir a carta, e colocar no vetor de abertos  */
-        /* Passo 3.2.1:  função para testar se as cartas possuem o mesmo emoji, se for igual manter viradas, se for diferentes desvirar a carta*/
-        /* Passo 3.2.2: Limpar vetor das abertas */
-/* Passo 4: Cria condição de vitoria */
+function levelDesign(lvl = level) {
+    const end = 16 * lvl;
+    let emojiSlice = emojis.slice(0, end);
+    let shuffledCards = emojiSlice.sort(() => (Math.random() > 0.5 ? 2 : -1));
+    console.log(shuffledCards);
+    cardAllocation(emojiSlice, shuffledCards);
+    return emojiSlice.length;
+}
+function elementCreate(
+    element_type = "button",
+    className = "",
+    innerHTML = "",
+    func = funcTest
+) {
+    const element = document.createElement(element_type);
+    element.className = className;
+    element.innerHTML = innerHTML;
+    element.onclick = func;
+    element.style += `background: #daa520; margin-left: 20px`;
+    return element;
+}
+
+function cardAllocation(emojiSliceVector, shuffledVector) {
+    for (let i = 0; i < emojiSliceVector.length; i++) {
+        let card = document.createElement("div");
+        card.className = "card";
+        card.innerHTML = shuffledVector[i];
+        card.onclick = cardHandleClick;
+        if (level > 1) {
+            card.style.width = `50px`;
+        }
+        document.querySelector("#tela").appendChild(card);
+    }
+    if (level == 1) {
+        tela.style.gap = "10px";
+    } else {
+        tela.style.gap = "4px";
+    }
+}
+
+function cardHandleClick() {
+    if (
+        openCards.length < 2 &&
+        document.querySelectorAll(".cardOpen").length < 2
+    ) {
+        if (!this.classList.contains("cardOpen", "cardMatched")) {
+            openCards.push(this);
+            this.classList.add("cardOpen");
+        }
+    }
+    if (openCards.length == 2) {
+        setTimeout(() => {
+            cardCheckMatch(openCards);
+        }, 500);
+    }
+}
+
+function cardCheckMatch(cards) {
+    if (cards[0].innerHTML === cards[1].innerHTML) {
+        console.log("Match: " + cards[0].innerHTML + "-" + cards[1].innerHTML);
+        cards[0].classList.add("cardMatched");
+        cards[1].classList.add("cardMatched");
+    }
+    cards[0].classList.remove("cardOpen");
+    cards[1].classList.remove("cardOpen");
+
+    openCards = [];
+    conditionVictory();
+}
+function conditionVictory() {
+    if (document.querySelectorAll(".cardMatched").length === matchsForVictory) {
+        tela.innerHTML = "";
+        const telaWin = tela.appendChild(youWin);
+        if (level < 3) {
+            youWin.appendChild(goToNextLevel);
+            menu.appendChild(button_level);
+        }
+    }
+}
+function nextLevel() {
+    youWin.removeChild(goToNextLevel);
+    tela.removeChild(youWin);
+    menu.removeChild(button_level);
+    level++;
+    matchsForVictory = levelDesign(level);
+}
+function funcTest() {}
+
+const tela = document.querySelector("#tela");
+const game = document.querySelector(".game");
+const menu = document.querySelector(".menu");
+const emojis = [
+    "🙈",
+    "🙈",
+    "🐸",
+    "🐸",
+    "🐔",
+    "🐔",
+    "🐺",
+    "🐺",
+    "🐼",
+    "🐼",
+    "🐷",
+    "🐷",
+    "🐱",
+    "🐱",
+    "🐶",
+    "🐶",
+    "🙉",
+    "🙉",
+    "🙊",
+    "🙊",
+    "🐵",
+    "🐵",
+    "🐯",
+    "🐯",
+    "🐮",
+    "🐮",
+    "🐗",
+    "🐗",
+    "🐭",
+    "🐭",
+    "🐹",
+    "🐹",
+    "🐰",
+    "🐰",
+    "🐻",
+    "🐻",
+    "🐨",
+    "🐨",
+    "😻",
+    "😻",
+    "😼",
+    "😼",
+    "😽",
+    "😽",
+    "🙀",
+    "🙀",
+    "😿",
+    "😿",
+];
+let openCards = [];
+let level = 1;
+let matchsForVictory = levelDesign(level);
+let button_level = elementCreate(
+    "button",
+    "next_level",
+    "Next Level",
+    nextLevel
+);
+
+let youWin = elementCreate("h1", "vitoria", "You Win!");
+let goToNextLevel = elementCreate(
+    "h2",
+    "goTo",
+    "Go to next Level..",
+    nextLevel
+);
